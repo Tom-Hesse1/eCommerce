16e8924f4a2c21d60985f29db9bf99be36adb824
hamaro01 <Katherine.helena01@gmail.com>
2020-07-07 18:46:14 -0400

    Add Product Model

---
+++ bin/README.md
@@ -0,0 +1,4 @@
+# eCommerce project for Code Immersion class
+
+
+Tom Hesse, Helen Amaro
---
+++ src/main/java/com/tts/eComm/model/Product.java
@@ -0,0 +1,8 @@
+package com.tts.eComm.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Product {
+
+}
