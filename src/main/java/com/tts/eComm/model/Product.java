55a872d95147290b79e7890d1c0511ab7f7dedf0
hamaro01 <Katherine.helena01@gmail.com>
2020-07-08 10:48:58 -0400

    Updated Product information

--- src/main/java/com/tts/eComm/model/Product.java
+++ src/main/java/com/tts/eComm/model/Product.java
@@ -1,7 +1,5 @@
 package com.tts.eComm.model;
-
 import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PositiveOrZero;
-
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -23,28 +20,130 @@ public class Product {
 	
 	@NotBlank(message = "Product name cannot be blank") //what is being sold?
 	private String name;
-
+	
 	@PositiveOrZero(message = "Product wholesale price cannot be negative") //but is zero really ok?
 	private Double wholesalePrice;
-
+	
 	@PositiveOrZero(message = "Product retail price cannot be negative") //but is zero really ok?
 	private Double retailPrice;
-
+	
 	@NotBlank(message = "Product brand cannot be blank") //somebody made it, right?
 	private String brand;
-
-	@NotBlank(message = "Product category cannot be blank")
-	private String category;
-
 	private String description; //is it ok to create a product and add a description later? or not?
-	private Integer inventory; //negative inventory doesn't really make sense, can't sell it if it isn't there 
-
+	private Integer inventory; //negative inventory doesn’t really make sense, can’t sell it if it isn’t there 
 	private String image; //is it ok to create a product and add an image file path later? or not?
-
+	
 	@CreationTimestamp
 	private Date createdAt;
-
+	
 	@UpdateTimestamp
 	private Date updatedAt;
-
+	
+	public Product() {
+	}
+	
+	public Product(@NotBlank(message = "Product name cannot be blank") String name,
+			@PositiveOrZero(message = "Product wholesale price cannot be negative") Double wholesalePrice,
+			@PositiveOrZero(message = "Product retail price cannot be negative") Double retailPrice,
+			@NotBlank(message = "Product brand cannot be blank") String brand, String description, Integer inventory,
+			String image, Date createdAt, Date updatedAt) {
+		this.name = name;
+		this.wholesalePrice = wholesalePrice;
+		this.retailPrice = retailPrice;
+		this.brand = brand;
+		this.description = description;
+		this.inventory = inventory;
+		this.image = image;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt; //???
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Double getWholesalePrice() {
+		return wholesalePrice;
+	}
+	
+	public void setWholesalePrice(Double wholesalePrice) {
+		this.wholesalePrice = wholesalePrice;
+	}
+	
+	public Double getRetailPrice() {
+		return retailPrice;
+	}
+	
+	public void setRetailPrice(Double retailPrice) {
+		this.retailPrice = retailPrice;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+	
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public Integer getInventory() {
+		return inventory;
+	}
+	
+	public void setInventory(Integer inventory) {
+		this.inventory = inventory;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+	
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", wholesalePrice=" + wholesalePrice + ", retailPrice="
+				+ retailPrice + ", brand=" + brand + ", description=" + description + ", inventory=" + inventory
+				+ ", image=" + image + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + "]";
+	}
+	
+	
+	
+	
+	
+	
 }
