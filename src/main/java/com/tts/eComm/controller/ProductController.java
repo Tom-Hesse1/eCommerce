d2d490ce8fe199e7befc776fa41d2f172296067e
hamaro01 <Katherine.helena01@gmail.com>
2020-07-08 12:05:36 -0400

    Product Controller file added

---
+++ src/main/java/com/tts/eComm/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.tts.eComm.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.tts.eComm.model.Product;
+import com.tts.eComm.service.ProductService;
+
+public class ProductController {
+	
+	@Autowired
+	private ProductService productService;
+	
+	@GetMapping("/product/{productId}")
+	public String show(@PathVariable Long id, Model model)
+	{
+		Product product = productService.findById(id);
+		model.addAttribute(product);
+		return "storefront/product";
+	}
+	
+	@RequestMapping(value = {"/products/new", "/products/{productId}/edit"}, method = {RequestMethod.POST, RequestMethod.PUT})
+	public String createOrUpdate(@Valid Product product)
+	{
+		productService.save(product);
+		return "redirect:/storefront/products/" + product.getId();
+	}
+
+}
