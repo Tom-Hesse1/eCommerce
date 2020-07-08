32db89e2a460dc41cdfcfb496f3f6f13a43a714c
hamaro01 <Katherine.helena01@gmail.com>
2020-07-08 11:05:07 -0400

    Add Product Repository

---
+++ src/main/java/com/tts/eComm/repository/ProductRepository.java
@@ -0,0 +1,28 @@
+package com.tts.eComm.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.tts.eComm.model.Product;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+	
+	public List<Product> findAll();
+	
+	public Product findById(long id);
+	
+	public List<Product> findByBrand(String brand);
+	
+	public List<Product> findByCategory(String category);
+	
+	public List<Product> findByBrandAndCategory(String brand, String category);
+
+	@Query("SELECT DISTINCT p.brand FROM Product p")
+	public List<String> findDistinctBrands();
+
+	@Query("SELECT DISTINCT p.category FROM Product p")
+	public List<String> findDistinctCategories();
+
+}
