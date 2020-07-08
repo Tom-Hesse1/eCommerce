c29569b9e45beb36366c388ac8400d6785ffb46c
hamaro01 <Katherine.helena01@gmail.com>
2020-07-08 11:39:20 -0400

    Product Service file added

---
+++ src/main/java/com/tts/eComm/service/ProductService.java
@@ -0,0 +1,60 @@
+package com.tts.eComm.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tts.eComm.model.Product;
+import com.tts.eComm.repository.ProductRepository;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+
+	public List<Product> findAll() 
+	{
+	  return productRepository.findAll();
+	}
+
+	public Product findById(long id) 
+	{
+	  return productRepository.findById(id);
+	}
+
+	public List<String> findBrands() 
+	{
+	  return productRepository.findDistinctBrands();
+	}
+
+	public List<String> findCategories() 
+	{
+	  return productRepository.findDistinctCategories();
+	}
+
+	public void save(Product product) 
+	{
+	  productRepository.save(product);
+	}
+
+	public void deleteById(long id) 
+	{
+	  productRepository.deleteById(id);
+	}
+
+	public List<Product> findByBrandAndOrCategory(String brand, String category) 
+	{
+	  List<Product> products = new ArrayList<>();
+	  if(category == null && brand == null) {
+	    products = productRepository.findAll();
+	  } else if(category == null) {
+	    products = productRepository.findByBrand(brand);
+	  } else if(brand == null) {
+	    products = productRepository.findByCategory(category);
+	  } else {
+	    products = productRepository.findByBrandAndCategory(brand, category);
+	  }
+	  return products;
+	}
+
+}
