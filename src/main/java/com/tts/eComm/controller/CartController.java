374996a6f706be6ec40e29923787d2f540f98ec6
Tom-Hesse1 <hesstruck64@gmail.com>
2020-07-10 14:12:42 -0400

    edited cart controller

--- src/main/java/com/tts/eComm/controller/CartController.java
+++ src/main/java/com/tts/eComm/controller/CartController.java
@@ -18,24 +18,13 @@ public class CartController {
 	@Autowired
 	private CartService cartService;
 
-	@GetMapping("/cart")
+	@GetMapping("/storefront/cart")
 	public String viewCart(Cart cart, Model model) {
-<<<<<<< HEAD
 	  model.addAttribute("cart", cart);
 	  return "storefront/cart";
 	}
 	
-	@PostMapping("/cart")
-	public String addToCart(@RequestParam long id) {
-		Product product = productService.findById(id);
-		setQuantity(product, cart().getOrDefault(product, 0) + 1);
-=======
-		model.addAttribute("cart", cart);
->>>>>>> fe7e5b8836266093689056a491082a163547bc8a
-		return "storefront/cart";
-	}
-
-	@PostMapping("/cart")
+	@PostMapping("/storefront/cart")
 	public String addToCart(@RequestParam Long productId, @RequestParam Integer quantity, Cart cart, Model model) {
 		cart = cartService.addLineItemToCart(cart, productId, quantity);
 		model.addAttribute("cart", cart);
