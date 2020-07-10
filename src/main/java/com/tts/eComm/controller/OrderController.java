02f95aafb4c635ba889d623492768852fe3050a7
hamaro01 <Katherine.helena01@gmail.com>
2020-07-10 15:53:28 -0400

    Updated Order Controller

--- src/main/java/com/tts/eComm/controller/OrderController.java
+++ src/main/java/com/tts/eComm/controller/OrderController.java
@@ -2,21 +2,32 @@ package com.tts.eComm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tts.eComm.model.Order;
 
 @Controller
+@RequestMapping("/storefront")
 public class OrderController {
 
 	@Autowired
 	private OrderService orderService;
 	
-	@GetMapping("/storefront/order/{orderId}")
-	public String show(PathVariable Long id, Model model)
+	@GetMapping("/order/{orderId}")
+	public String show(@PathVariable Long id, Model model)
 	{
 		Order order = orderService.findOrderById(id);
 		model.addAttribute(order);
 		return "storefront/order";
 	}
+	
+	@GetMapping("/order")
+	  public String getOrderPage(){
+	    //code
+	    return "storefront/order";
+	  }
 }
 
