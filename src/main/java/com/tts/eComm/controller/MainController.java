db5b1633b44ec582ac8059c71bb89d326320459f
hamaro01 <Katherine.helena01@gmail.com>
2020-07-12 21:34:03 -0400

    Update Main Controller

--- src/main/java/com/tts/eComm/controller/MainController.java
+++ src/main/java/com/tts/eComm/controller/MainController.java
@@ -6,18 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tts.eComm.model.Product;
 import com.tts.eComm.service.ProductService;
-import com.tts.eComm.model.Cart;
 
 @Controller
 public class MainController {
 
 	@Autowired
 	private ProductService productService;
-
+	
+	@GetMapping("/")
+    public String main() {
+        return "main";
+    }
+	
 	@GetMapping(value = { "/", "/home" })
 	public String home(Model model) {
 		model.addAttribute("products", productService.findAll());
